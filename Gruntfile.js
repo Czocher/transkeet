2c8e5a9962d745d99a458f7eb713f8e51066772e
Paweł Jan Czochański <czochanski@gmail.com>
2016-04-01 21:49:28 +0200

    Added grunt-karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,22 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
 
   grunt.registerTask('default', ['jshint']);
 
